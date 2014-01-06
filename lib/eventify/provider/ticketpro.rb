17436fdfb336df61764079e94f3760479230abc2
Jarmo Pertman <jarmo.p@gmail.com>
2014-01-06 19:27:23 +0200

    Strip localization info from Ticketpro id and link already in #initialize.

--- lib/eventify/provider/ticketpro.rb
+++ lib/eventify/provider/ticketpro.rb
@@ -5,17 +5,17 @@ module Eventify::Provider
   class Ticketpro < Base
     URL = "http://www.ticketpro.ee/jnp/rss/index.xml" 
 
+    def initialize(event)
+      super
+      @id = @id.gsub(%r{/jnp/(ru|en)/}, "/jnp/")
+      @link = @link.gsub(%r{/jnp/(ru|en)/}, "/jnp/")
+    end
+
     class << self
       def fetch
         rss = SimpleRSS.parse open(URL)
         rss.entries.map { |entry| new id: entry.guid, title: entry.title, link: entry.link, date: entry.pubDate }
       end
-    end
-
-    alias_method :_id, :id
-
-    def id
-      _id.gsub(%r{/jnp/(ru|en)/}, "/jnp/")
     end
   end
 end
--- spec/eventify/provider/ticketpro_spec.rb
+++ spec/eventify/provider/ticketpro_spec.rb
@@ -1,6 +1,45 @@
 require "spec_helper"
 
 describe Eventify::Provider::Ticketpro do
+
+  context "#initialize" do
+    it "strips en localization info from id" do
+      Eventify::Provider::Ticketpro.new(
+        title: "Danny Malando tantsumuusika orkester ( Holland )",
+        link: "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html",
+        date: Time.parse("Fri, 31 May 2013 14:29:07 +0200"),
+        id: "http://www.ticketpro.ee/jnp/en/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
+      ).id.should == "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
+    end
+
+    it "strips ru localization info from id" do
+      Eventify::Provider::Ticketpro.new(
+        title: "Danny Malando tantsumuusika orkester ( Holland )",
+        link: "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html",
+        date: Time.parse("Fri, 31 May 2013 14:29:07 +0200"),
+        id: "http://www.ticketpro.ee/jnp/ru/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
+      ).id.should == "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
+    end
+
+    it "strips en localization info from link" do
+      Eventify::Provider::Ticketpro.new(
+        title: "Danny Malando tantsumuusika orkester ( Holland )",
+        link: "http://www.ticketpro.ee/jnp/en/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html",
+        date: Time.parse("Fri, 31 May 2013 14:29:07 +0200"),
+        id: "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
+      ).link.should == "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
+    end
+
+    it "strips ru localization info from link" do
+      Eventify::Provider::Ticketpro.new(
+        title: "Danny Malando tantsumuusika orkester ( Holland )",
+        link: "http://www.ticketpro.ee/jnp/ru/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html",
+        date: Time.parse("Fri, 31 May 2013 14:29:07 +0200"),
+        id: "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
+      ).link.should == "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
+    end
+  end
+
   context "#fetch" do
     it "fetches events" do
       stub_request(:get, Eventify::Provider::Ticketpro::URL).to_return(body: File.read(File.expand_path("../../support/ticketpro.xml", __dir__)))
@@ -20,73 +59,6 @@ describe Eventify::Provider::Ticketpro do
           id: "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
         )
       ]
-    end
-  end
-
-  context "#==" do
-    it "true between non-localized and ru localized event" do
-      not_localized_event = Eventify::Provider::Ticketpro.new(
-        title: "Danny Malando tantsumuusika orkester ( Holland )",
-        link: "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html",
-        date: Time.parse("Fri, 31 May 2013 14:29:07 +0200"),
-        id: "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
-      )
-
-      ru_localized_event = Eventify::Provider::Ticketpro.new(
-        title: "Danny Malando tantsumuusika orkester ( Holland )",
-        link: "http://www.ticketpro.ee/jnp/ru/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html",
-        date: Time.parse("Fri, 31 May 2013 14:29:07 +0200"),
-        id: "http://www.ticketpro.ee/jnp/ru/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
-      )
-
-      not_localized_event.should == ru_localized_event
-    end
-
-    it "true between non-localized and en localized event" do
-      not_localized_event = Eventify::Provider::Ticketpro.new(
-        title: "Danny Malando tantsumuusika orkester ( Holland )",
-        link: "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html",
-        date: Time.parse("Fri, 31 May 2013 14:29:07 +0200"),
-        id: "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
-      )
-
-      en_localized_event = Eventify::Provider::Ticketpro.new(
-        title: "Danny Malando tantsumuusika orkester ( Holland )",
-        link: "http://www.ticketpro.ee/jnp/en/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html",
-        date: Time.parse("Fri, 31 May 2013 14:29:07 +0200"),
-        id: "http://www.ticketpro.ee/jnp/en/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
-      )
-
-      not_localized_event.should == en_localized_event
-    end
-
-    it "true between ru localized and en localized event" do
-      ru_localized_event = Eventify::Provider::Ticketpro.new(
-        title: "Danny Malando tantsumuusika orkester ( Holland )",
-        link: "http://www.ticketpro.ee/jnp/ru/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html",
-        date: Time.parse("Fri, 31 May 2013 14:29:07 +0200"),
-        id: "http://www.ticketpro.ee/jnp/ru/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
-      )      
-
-      en_localized_event = Eventify::Provider::Ticketpro.new(
-        title: "Danny Malando tantsumuusika orkester ( Holland )",
-        link: "http://www.ticketpro.ee/jnp/en/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html",
-        date: Time.parse("Fri, 31 May 2013 14:29:07 +0200"),
-        id: "http://www.ticketpro.ee/jnp/en/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
-      )
-
-      ru_localized_event.should == en_localized_event
-    end
-  end
-
-  context "#id" do
-    it "strips out localized part of url" do
-      Eventify::Provider::Ticketpro.new(
-        title: "Danny Malando tantsumuusika orkester ( Holland )",
-        link: "http://www.ticketpro.ee/jnp/en/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html",
-        date: Time.parse("Fri, 31 May 2013 14:29:07 +0200"),
-        id: "http://www.ticketpro.ee/jnp/en/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
-      ).id.should == "http://www.ticketpro.ee/jnp/music/1095688-danny-malando-tantsumuusika-orkester-holland-.html"
     end
   end
 end
